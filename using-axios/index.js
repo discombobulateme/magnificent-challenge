8bb116d8ebf90177d4a92417a5da32ab59f37b4c
discombobulateme <p@discombobulate.me>
2020-09-13 13:58:22 +0200

    Change function order as async is being used

--- using-axios/index.js
+++ using-axios/index.js
@@ -1,24 +1,7 @@
 // this files get the server requests
 const axios = require("axios");
 
-// 1. get user input from terminal to start application
-// how to do it from terminal?
-// 2. get user input in seconds
-// 3. transform user input to miliseconds
-
 // define the server check interval time
-function checkServer() {
-  interval = setInterval(() => {}, intervalTime);
-}
-
-// 4. access server response
-
-async function makeHeadRequest() {
-  let res = await axios.head("http://localhost:12345");
-
-  console.log(`Status: ${res.status}`);
-  console.log(`Server: ${res.headers.server}`);
-  console.log(`Date: ${res.headers.date}`);
 }
 
 makeHeadRequest();
