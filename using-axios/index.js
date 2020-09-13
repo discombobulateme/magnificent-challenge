9583c276f928b8ed393729d9b58b38b070044414
discombobulateme <p@discombobulate.me>
2020-09-13 11:30:50 +0200

    Add a basic axios app model as comment

--- using-axios/index.js
+++ using-axios/index.js
@@ -22,3 +22,23 @@ async function makeHeadRequest() {
 }
 
 makeHeadRequest();
+
+/*
+// axios basic api
+const axios = require('axios');
+
+async function makeRequest() {
+
+    const config = {
+        method: 'get',
+        url: 'http://webcode.me'
+    }
+
+    let res = await axios(config)
+
+    console.log(res.status);
+}
+
+makeRequest();
+*/
+
