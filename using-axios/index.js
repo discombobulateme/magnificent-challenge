036d477f0b509171d19b0d44101666f9f95112b7
discombobulateme <p@discombobulate.me>
2020-09-13 11:29:23 +0200

    Add steps as comment to be implemented

--- using-axios/index.js
+++ using-axios/index.js
@@ -1,6 +1,11 @@
 // this files get the server requests
 const axios = require("axios");
 
+// 1. get user input from terminal to start application
+// how to do it from terminal?
+// 2. get user input in seconds
+// 3. transform user input to miliseconds
+
 async function makeHeadRequest() {
   let res = await axios.head("http://localhost:12345");
 
