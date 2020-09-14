ae35ff464b3ecbedfcec15870d800c55685a5656
discombobulateme <p@discombobulate.me>
2020-09-14 16:41:49 +0200

    Add .finally to fix message order only after server check count

--- using-axios/index.js
+++ using-axios/index.js
@@ -13,6 +13,10 @@ const interval = (process.argv[2] || 1) * 1000;
 // get user parameter from terminal to start application
 // index 2 = argument -> process.argv [application = node, file = index.js, argument]
 checkServer(process.argv[2]);
+  } finally {
+    // add a finally to make sure log is in the async line
+    console.log(`Total of successes = ${successes}, total fails = ${fails}`);
+  }
 
 // [] Exporting checkServer() for unit test framework
 // [] In order to do that checkServer should not be called immediately, only if (require.main === module) { (https://nodejs.org/api/modules.html#modules_accessing_the_main_module)
