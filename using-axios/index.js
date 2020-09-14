9ae255e0929c46506b8294da014c049ffb1d6570
discombobulateme <p@discombobulate.me>
2020-09-14 16:42:36 +0200

    Change function to try catch to anulate promise and make async code

--- using-axios/index.js
+++ using-axios/index.js
@@ -6,17 +6,26 @@ const interval = (process.argv[2] || 1) * 1000;
 // define the server check interval time
 
 
+setInterval(async () => {
+  // access server response
+  try {
+    // get just the header (return) and not the full body
+    // [] add variable to the url
+    await axios.head("http://localhost:12345");
     makeServerHeadRequest();
   }, intervalTime);
 }
 
-// get user parameter from terminal to start application
-// index 2 = argument -> process.argv [application = node, file = index.js, argument]
-checkServer(process.argv[2]);
+    successes++;
+    console.log("Success!!! 🥳");
+  } catch (error) {
+    console.log("Failed 😟 😢");
+    fails++;
   } finally {
     // add a finally to make sure log is in the async line
     console.log(`Total of successes = ${successes}, total fails = ${fails}`);
   }
+}, interval);
 
 // [] Exporting checkServer() for unit test framework
 // [] In order to do that checkServer should not be called immediately, only if (require.main === module) { (https://nodejs.org/api/modules.html#modules_accessing_the_main_module)
