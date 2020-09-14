c6eca2ce6a425c9f1ea717dd30e78c907b6a914c
discombobulateme <p@discombobulate.me>
2020-09-14 16:37:24 +0200

    Delete promisse make it async

--- using-axios/index.js
+++ using-axios/index.js
@@ -2,28 +2,7 @@ const axios = require("axios");
 const process = require("process");
 
 // define the server check interval time
-function checkServer(userIntervalTime) {
-  // transforms user parameter in seconds into setInterval miliseconds, as needed
-  intervalTime = userIntervalTime * 1000;
 
-  let fails = 0;
-  let successes = 0;
-
-  setInterval(() => {
-    // access server response
-    async function makeServerHeadRequest() {
-      axios
-        .get("http://localhost:12345")
-        .then((response) => {
-          successes++;
-          console.log("Success!!! 🥳");
-        })
-        .catch((error) => {
-          console.log("Failed 😟 😢");
-          fails++;
-        });
-      console.log(`Total of successes = ${successes}, total fails = ${fails}`);
-    }
 
     makeServerHeadRequest();
   }, intervalTime);
